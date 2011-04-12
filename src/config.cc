b320d3cd523a99fabd7d05b2b0f9c754b2da44f0
lotem <chen.sst@gmail.com>
2011-04-12 15:53:52 +0000

    defined config item
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/librime@50 b90aed45-4297-9963-ff9d-98b604ac09a9

--- src/config.cc
+++ src/config.cc
@@ -7,6 +7,7 @@
 // 2011-4-6 Zou xu <zouivex@gmail.com>
 //
 #include <fstream>
+#include <boost/filesystem.hpp>
 #include "config.h"
 
 namespace rime {
@@ -20,14 +21,40 @@ void YamlConfig::LoadFromFile(const std::string& file_name) {
 void YamlConfig::SaveToFile(const std::string& file_name) {
 }
 
-const std::string YamlConfig::GetValue(const std::string& key) {
+const bool YamlConfig::GetBool(const std::string& key) {
+  // TODO(zouxu):
+  return false;
+}
+
+const int YamlConfig::GetInt(const std::string& key) {
+  // TODO(zouxu):
+  return 0;
+}
+
+const double YamlConfig::GetDouble(const std::string& key) {
+  // TODO(zouxu):
+  return 0.0;
+}
+
+const std::string YamlConfig::GetString(const std::string& key) {
   // TODO(zouxu):
   return "";
 }
 
+ConfigList* YamlConfig::GetList(const std::string& key) {
+  // TODO(zouxu):
+  return NULL;
+}
+
+ConfigMap* YamlConfig::GetMap(const std::string& key) {
+  // TODO(zouxu):
+  return NULL;
+}
+
 YamlConfig* YamlConfigComponent::Create(const std::string &file_name) {
-  // TODO: use boost::filesystem's path join function
-  return new YamlConfig(conf_dir_ + file_name);
+  boost::filesystem::path p(conf_dir_);
+  p /= file_name;
+  return new YamlConfig(p.string());
 }
 
 }  // namespace rime
--- src/config.h
+++ src/config.h
@@ -16,11 +16,56 @@
 
 namespace rime {  
 
-class Config : public Class_<Config, std::string> {
- public:
-  virtual const std::string GetValue(const std::string &key) = 0;
+// type of config values
+enum ConfigValueType {
+  kNull = 0,
+  kBool,
+  kNumber,
+  kString,
+  kList,
+  kMap,
+  kLastType
 };
 
+// config item base class
+class ConfigItem {
+ public:
+  virtual bool AsBool() const = 0;
+  virtual int AsInt() const = 0;
+  virtual double AsDouble() const = 0;
+  virtual std::string AsString() const = 0;
+  ConfigValueType type() const { return type_; }
+  // TODO: static assert that class T inherits from ConfigItem
+  template <class T>
+  T* As() const { return dynamic_cast<T>(this); }
+ protected:
+  ConfigValueType type_;
+};
+
+class ConfigList : public ConfigItem {
+ public:
+  virtual ConfigItem* Get(size_t n) const = 0;
+  virtual size_t length() const = 0;
+};
+
+class ConfigMap : public ConfigItem {
+ public:
+  virtual bool HasKey(const std::string &key) const = 0;
+  virtual ConfigItem* Get(const std::string &key) const = 0;
+};
+
+// config component interface
+class Config : public Class_<Config, std::string> {
+ public:
+  virtual const bool GetBool(const std::string &key) = 0;
+  virtual const int GetInt(const std::string &key) = 0;
+  virtual const double GetDouble(const std::string &key) = 0;
+  virtual const std::string GetString(const std::string &key) = 0;
+  virtual ConfigList* GetList(const std::string &key) = 0;
+  virtual ConfigMap* GetMap(const std::string &key) = 0;
+};
+
+// a config implementation using yaml files
 class YamlConfig : public Config {
  public:
   YamlConfig() {}
@@ -31,12 +76,19 @@ class YamlConfig : public Config {
 
   void LoadFromFile(const std::string& file_name);
   void SaveToFile(const std::string& file_name);
-  const std::string GetValue(const std::string& key);
+
+  virtual const bool GetBool(const std::string &key);
+  virtual const int GetInt(const std::string &key);
+  virtual const double GetDouble(const std::string &key);
+  virtual const std::string GetString(const std::string &key);
+  virtual ConfigList* GetList(const std::string &key);
+  virtual ConfigMap* GetMap(const std::string &key);
 
  private:
   YAML::Node doc_;
 };
 
+// the component implementation
 class YamlConfigComponent : public Component_<YamlConfig> {
  public:
   YamlConfigComponent(const std::string &conf_dir) : conf_dir_(conf_dir) {}
