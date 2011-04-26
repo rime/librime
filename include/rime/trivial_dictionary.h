9fa5fcc59bca2a174d30542704bff1942901e1ee
lotem <chen.sst@gmail.com>
2011-04-26 13:50:15 +0000

    trivial_dictionary.h
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/librime@72 b90aed45-4297-9963-ff9d-98b604ac09a9

---
+++ include/rime/trivial_dictionary.h
@@ -0,0 +1,27 @@
+// vim: set sts=2 sw=2 et:
+// encoding: utf-8
+//
+// Copyleft 2011 RIME Developers
+// License: GPLv3
+//
+// 2011-04-26 Wensong He <snowhws@gmail.com>
+//
+
+#ifndef TRIVIAL_DICTIONARY_H_
+#define TRIVIAL_DICTIONARY_H_
+
+namespace rime {
+  
+class TrivialDictionary {
+  public:
+    
+  private:
+    //Vocabulary *vacabulary_ptr_;
+    
+};
+
+} //  namespace rime
+
+#endif  //  TRIVIAL_DICTIONARY_H_
+
+
