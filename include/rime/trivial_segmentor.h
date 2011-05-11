21a611cc5f16e1feffe0ee27f9e116f5694ea893
lotem <chen.sst@gmail.com>
2011-05-11 01:14:15 +0000

    git-svn-id: https://rimeime.googlecode.com/svn/trunk/librime@103 b90aed45-4297-9963-ff9d-98b604ac09a9

---
+++ include/rime/trivial_segmentor.h
@@ -0,0 +1,34 @@
+// vim: set sts=2 sw=2 et:
+// encoding: utf-8
+//
+// Copyleft 2011 RIME Developers
+// License: GPLv3
+//
+// 2011-05-11 Wensong He <snowhws@gmail.com>
+//
+
+#ifndef TRIVIAL_SEGMENTOR_H_
+#define TRIVIAL_SEGMENTOR_H_
+
+#include <rime/segment.h>
+
+namespace rime {
+
+class TrivialSegmentor : public Segmentor {
+ public:
+  TrivialSegmentor(){};
+  TrivialSegmentor(const Schema *schema) : schema_(schema) {}
+  //@input the string need to segment
+  //@result result pointer of segmenting 
+  virtual bool segment(std::string input,void *result);
+
+ protected:
+};
+
+}  // namespace rime
+
+#endif  // SEGMENTOR_H_
+
+
+
+
