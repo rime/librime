91f9b51a01813f74ae3d2442620c33f675445ac8
lotem <chen.sst@gmail.com>
2011-05-11 01:25:04 +0000

    git-svn-id: https://rimeime.googlecode.com/svn/trunk/librime@109 b90aed45-4297-9963-ff9d-98b604ac09a9

---
+++ src/trivial_vocabulary.cc
@@ -0,0 +1,23 @@
+// vim: set sts=2 sw=2 et:
+// encoding: utf-8
+//
+// Copyleft 2011 RIME Developers
+// License: GPLv3
+// 
+// 2011-05-11 Wensong He <snowhws@gmail.com>
+//
+
+#include <rime/trivial_vocabulary.h>
+
+namespace rime {
+
+/*void TrivialVocabulary::FindWord() {
+  //input.split
+  //result->***
+  //result->***
+  //return true
+}*/
+
+}  // namespace rime
+
+
