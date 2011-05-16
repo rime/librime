0957221c31482c99a014787885545e74c916c9cc
zouivex <zouivex@gmail.com>
2011-05-16 13:29:51 +0000

    Start working on the trie system...
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/librime@119 b90aed45-4297-9963-ff9d-98b604ac09a9

---
+++ include/rime/dtrie.h
@@ -0,0 +1,24 @@
+// vim: set sts=2 sw=2 et:
+// encoding: utf-8
+//
+// Copyleft 2011 RIME Developers
+// License: GPLv3
+// 
+// 2011-5-16 Zou xu <zouivex@gmail.com>
+//
+
+#ifndef RIME_DTRIE_H_
+#define RIME_DTRIE_H_
+
+#include <darts.h>
+
+namespace rime {
+
+//
+class dtrie {
+
+};
+
+}  // namespace rime
+
+#endif  // RIME_DTRIE_H_
---
+++ src/dtrie.cc
@@ -0,0 +1,14 @@
+// vim: set sts=2 sw=2 et:
+// encoding: utf-8
+//
+// Copyleft 2011 RIME Developers
+// License: GPLv3
+// 
+// 2011-5-16 Zou xu <zouivex@gmail.com>
+//
+
+#include <rime/dtrie.h>
+
+namespace rime{
+
+}  // namespace rime
