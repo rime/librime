ff4cdcf5c40fcfde1270fc9b57d5643f012fb8c8
lotem <chen.sst@gmail.com>
2011-07-10 08:47:44 +0000

    Add the missing file.
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/librime@172 b90aed45-4297-9963-ff9d-98b604ac09a9

---
+++ include/rime/impl/vocabulary.h
@@ -0,0 +1,45 @@
+// vim: set sts=2 sw=2 et:
+// encoding: utf-8
+//
+// Copyleft 2011 RIME Developers
+// License: GPLv3
+// 
+// 2011-07-10 GONG Chen <chen.sst@gmail.com>
+//
+
+#ifndef RIME_VOCABULARY_H_
+#define RIME_VOCABULARY_H_
+
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+#include <rime/common.h>
+
+namespace rime {
+
+typedef std::set<std::string> Syllabary;
+
+class Code : public std::vector<int> {
+ public:
+  static const size_t kIndexCodeMaxLength = 3;
+  
+  bool operator< (const Code &other) const;
+  bool operator== (const Code &other) const;
+  
+  void CreateIndex(Code* index_code);
+};
+
+struct DictEntry { 
+  Code code;
+  std::string text;
+  double weight;
+  bool operator< (const DictEntry& other) const;
+};
+
+typedef std::vector<DictEntry> DictEntryList;
+typedef std::map<Code, DictEntryList> Vocabulary;
+
+}  // namespace rime
+
+#endif  // RIME_VOCABULARY_H_
