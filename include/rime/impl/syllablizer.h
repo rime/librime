d9e8c072a2ab1d36773737027238d4fa1dd473c6
zouivex <zouivex@gmail.com>
2011-07-12 13:46:18 +0000

    syllablizer? maybe.
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/librime@179 b90aed45-4297-9963-ff9d-98b604ac09a9

---
+++ include/rime/impl/syllablizer.h
@@ -0,0 +1,20 @@
+// vim: set sts=2 sw=2 et:
+// encoding: utf-8
+//
+// Copyleft 2011 RIME Developers
+// License: GPLv3
+// 
+// 2011-7-12 Zou xu <zouivex@gmail.com>
+//
+
+#ifndef RIME_SYLLABLIZER_H_
+#define RIME_SYLLABLIZER_H_
+
+namespace rime {
+
+class Syllablizer {
+};
+
+}  // namespace rime
+
+#endif  // RIME_SYLLABLIZER_H_
---
+++ src/impl/syllablizer.cc
@@ -0,0 +1,13 @@
+// vim: set sts=2 sw=2 et:
+// encoding: utf-8
+//
+// Copyleft 2011 RIME Developers
+// License: GPLv3
+// 
+// 2011-7-12 Zou xu <zouivex@gmail.com>
+//
+#include <rime/impl/syllablizer.h>
+
+namespace rime {
+
+}  // namespace rime
