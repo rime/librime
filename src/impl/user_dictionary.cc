7fb4e5e98d2521747cc097bab2f2c3a9eaca0c16
lotem <chen.sst@gmail.com>
2011-11-07 14:52:37 +0000

    Confused with the buffers.
    git-svn-id: https://rimeime.googlecode.com/svn/trunk/librime@307 b90aed45-4297-9963-ff9d-98b604ac09a9

--- src/impl/user_dictionary.cc
+++ src/impl/user_dictionary.cc
@@ -220,11 +220,14 @@ UserDictionaryComponent::UserDictionaryComponent() {
 UserDictionary* UserDictionaryComponent::Create(Schema *schema) {
   if (!schema) return NULL;
   Config *config = schema->config();
-  const std::string &schema_id(schema->schema_id());
+  bool enable_user_dict = true;
+  config->GetBool("translator/enable_user_dict", &enable_user_dict);
+  if (!enable_user_dict)
+    return NULL;
   std::string dict_name;
   if (!config->GetString("translator/dictionary", &dict_name)) {
     EZLOGGERPRINT("Error: dictionary not specified in schema '%s'.",
-                  schema_id.c_str());
+                  schema->schema_id().c_str());
     return NULL;
   }
   // obtain userdb object
