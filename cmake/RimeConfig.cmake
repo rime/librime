b9061a1ce8bb7fd79cce42958b2a6a0813fcd778
lotem <chen.sst@gmail.com>
2013-05-05 20:17:12 +0800

    static build.

--- cmake/RimeConfig.cmake
+++ cmake/RimeConfig.cmake
@@ -1,8 +1,8 @@
 # - Locate the rime library
 # This module defines
 #  Rime_FOUND found librime
-#  Rime_LIBRARY rime library path
-#  Rime_INCLUDE_DIRS, where to find headers
+#  Rime_LIBRARIES rime library path
+#  Rime_INCLUDE_DIR, where to find headers
 
 #==============================================================================
 # Copyright 2012 Xuetian Weng
@@ -18,19 +18,18 @@ if(Rime_INCLUDE_DIR AND Rime_LIBRARIES)
     set(Rime_FIND_QUIETLY TRUE)
 endif(Rime_INCLUDE_DIR AND Rime_LIBRARIES)
 
-#include(FindPkgConfig)
-#PKG_CHECK_MODULES(PC_Rime fcitx)
+include(FindPkgConfig)
+PKG_CHECK_MODULES(PC_Rime rime)
 
-find_path(Rime_MAIN_INCLUDE_DIR
+find_path(Rime_INCLUDE_DIR
           NAMES rime_api.h
           HINTS ${PC_Rime_INCLUDEDIR}
           )
 
 find_library(Rime_LIBRARIES
              NAMES rime
-             HINTS ${PC_Rime_LIBDIR})
-
-set(Rime_INCLUDE_DIR "${Rime_MAIN_INCLUDE_DIR}")
+             HINTS ${PC_Rime_LIBDIR}
+             )
 
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Rime DEFAULT_MSG Rime_LIBRARIES Rime_MAIN_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Rime DEFAULT_MSG Rime_LIBRARIES Rime_INCLUDE_DIR)
