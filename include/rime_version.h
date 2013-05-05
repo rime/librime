12b4dbc30bfa4b796636e15bc4f37b32878353c0
lotem <chen.sst@gmail.com>
2013-05-05 21:36:57 +0800

    Rime 0.9.9 release.

--- include/rime_version.h
+++ include/rime_version.h
@@ -1,5 +1,5 @@
 /*
- * Copyleft 2011 RIME Developers
+ * Copyleft RIME Developers
  * License: GPLv3
  *
  * 2011-12-04 GONG Chen <chen.sst@gmail.com>
@@ -7,6 +7,6 @@
 #ifndef RIME_VERSION_H_
 #define RIME_VERSION_H_
 
-#define RIME_VERSION "0.9.8"
+#define RIME_VERSION "0.9.9"
 
 #endif  // RIME_VERSION_H_
