67ef88ebf3a4750ebc947099807ab2bed641d5f2
lotem <chen.sst@gmail.com>
2013-07-22 01:19:22 +0800

    encoder;

---
+++ include/rime/algo/encoder.h
@@ -0,0 +1,79 @@
+//
+// Copyleft RIME Developers
+// License: GPLv3
+//
+// 2013-07-17 GONG Chen <chen.sst@gmail.com>
+//
+
+#ifndef RIME_ENCODER_H_
+#define RIME_ENCODER_H_
+
+#include <set>
+#include <string>
+#include <vector>
+#include <boost/regex.hpp>
+
+namespace rime {
+
+/*
+# sample encoder configuration (from cangjie5.dict.yaml)
+encoder:
+  exclude_patterns:
+  - '^x.*$'
+  - '^z.*$'
+  rules:
+  - length_equal: 2
+    formula: "AaAzBaBbBz"
+  - length_equal: 3
+    formula: "AaAzBaBzCz"
+  - length_in_range: [4, 10]
+    formula: "AaBzCaYzZz"
+  tail_anchor: "'"
+*/
+
+// Aa : code at index 0 for character at index 0
+// Az : code at index -1 for character at index 0
+// Za : code at index 0 for character at index -1
+struct CodeCoords {
+  int char_index;
+  int code_index;
+};
+
+struct TableEncodingRule {
+  int min_word_length;
+  int max_word_length;
+  std::vector<CodeCoords> coords;
+};
+
+class TableEncoder {
+ public:
+  void LoadSettings(Config* config);
+  bool Encode(const std::vector<std::string>& code, std::string* result);
+  std::set<std::string> EncodePhrase(const std::string& phrase);
+
+  bool IsCodeExcluded(const std::string& code);
+  const std::vector<TableEncodingRule>& encoding_rules() const {
+    return encoding_rules_;
+  }
+  const std::string& tail_anchor() const { return tail_anchor_; }
+
+ protected:
+  bool ParseFormula(const std::string& formula, TableEncodingRule* rule);
+  // index: 0-based virtual index of encoding characters in `code`.
+  //        counting from the end of `code` if `index` is negative.
+  //        tail anchors do not count as encoding characters.
+  // start: when `index` is negative, the first appearance of a tail anchor
+  //        beyond `start` is used to locate the encoding character at index -1.
+  // returns string index in `code` for the character at virtual `index`.
+  // may return a negative number if `index` does not exist in `code`.
+  int CalculateCodeIndex(const std::string& code, int index, int start);
+
+  // settings
+  std::vector<TableEncodingRule> encoding_rules_;
+  std::vector<boost::regex> exclude_patterns_;
+  std::string tail_anchor_;
+};
+
+}  // namespace rime
+
+#endif  // RIME_ENCODER_H_
---
+++ src/algo/encoder.cc
@@ -0,0 +1,188 @@
+//
+// Copyleft RIME Developers
+// License: GPLv3
+//
+// 2013-07-17 GONG Chen <chen.sst@gmail.com>
+//
+#include <boost/foreach.hpp>
+#include <rime/config.h>
+#include <rime/algo/encoder.h>
+
+namespace rime {
+
+void TableEncoder::LoadSettings(Config* config) {
+  encoding_rules_.clear();
+  exclude_patterns_.clear();
+  tail_anchor_.clear();
+  if (!config) return;
+  if (ConfigListPtr rules = config->GetList("encoder/rules")) {
+    for (ConfigList::Iterator it = rules->begin(); it != rules->end(); ++it) {
+      ConfigMapPtr rule = As<ConfigMap>(*it);
+      if (!rule || !rule->HasKey("formula"))
+        continue;
+      const std::string formula(rule->GetValue("formula")->str());
+      TableEncodingRule r;
+      if (!ParseFormula(formula, &r))
+        continue;
+      r.min_word_length = r.max_word_length = 0;
+      if (ConfigValuePtr value = rule->GetValue("length_equal")) {
+        int length = 0;
+        if (!value->GetInt(&length)) {
+          LOG(ERROR) << "invalid length";
+          continue;
+        }
+        r.min_word_length = r.max_word_length = length;
+      }
+      else if (ConfigListPtr range =
+               As<ConfigList>(rule->Get("length_in_range"))) {
+        if (range->size() != 2 ||
+            !range->GetValueAt(0) ||
+            !range->GetValueAt(1) ||
+            !range->GetValueAt(0)->GetInt(&r.min_word_length) ||
+            !range->GetValueAt(1)->GetInt(&r.max_word_length) ||
+            r.min_word_length > r.max_word_length) {
+          LOG(ERROR) << "invalid range.";
+          continue;
+        }
+      }
+      encoding_rules_.push_back(r);
+    }
+  }
+  if (ConfigListPtr excludes = config->GetList("encoder/exclude_patterns")) {
+    for (ConfigList::Iterator it = excludes->begin();
+         it != excludes->end(); ++it) {
+      ConfigValuePtr pattern = As<ConfigValue>(*it);
+      if (!pattern)
+        continue;
+      exclude_patterns_.push_back(boost::regex(pattern->str()));
+    }
+  }
+  config->GetString("encoder/tail_anchor", &tail_anchor_);
+}
+
+bool TableEncoder::ParseFormula(const std::string& formula,
+                                TableEncodingRule* rule) {
+  if (formula.length() % 2 != 0) {
+    LOG(ERROR) << "bad formula: '%s'" << formula;
+    return false;
+  }
+  for (std::string::const_iterator it = formula.begin(), end = formula.end();
+       it != end; ) {
+    CodeCoords c;
+    if (*it < 'A' || *it > 'Z') {
+      LOG(ERROR) << "invalid character index in formula: '%s'" << formula;
+      return false;
+    }
+    c.char_index = (*it >= 'U') ? (*it - 'Z' - 1) : (*it - 'A');
+    ++it;
+    if (*it < 'a' || *it > 'z') {
+      LOG(ERROR) << "invalid code index in formula: '%s'" << formula;
+      return false;
+    }
+    c.code_index = (*it >= 'u') ? (*it - 'z' - 1) : (*it - 'a');
+    ++it;
+    rule->coords.push_back(c);
+  }
+  return true;
+}
+
+bool TableEncoder::IsCodeExcluded(const std::string& code) {
+  BOOST_FOREACH(const boost::regex& pattern, exclude_patterns_) {
+    if (boost::regex_match(code, pattern))
+      return true;
+  }
+  return false;
+}
+
+bool TableEncoder::Encode(const std::vector<std::string>& code,
+                          std::string* result) {
+  BOOST_FOREACH(const TableEncodingRule& rule, encoding_rules_) {
+    if (code.size() < rule.min_word_length ||
+        code.size() > rule.max_word_length) {
+      continue;
+    }
+    result->clear();
+    CodeCoords previous = {0, 0};
+    CodeCoords encoded = {0, 0};
+    BOOST_FOREACH(const CodeCoords& current, rule.coords) {
+      CodeCoords c(current);
+      if (c.char_index < 0) {
+          c.char_index += static_cast<int>(code.size());
+      }
+      if (c.char_index >= static_cast<int>(code.size())) {
+        continue;  // 'abc def' ~ 'Ca'
+      }
+      if (c.char_index < 0) {
+        continue;  // 'abc def' ~ 'Xa'
+      }
+      if (current.char_index < 0 &&
+          c.char_index < encoded.char_index) {
+        continue;  // 'abc def' ~ '(AaBa)Ya'
+        // 'abc def' ~ '(AaBa)Aa' is OK
+      }
+      int start_index = 0;
+      if (c.char_index == encoded.char_index) {
+        start_index = encoded.code_index;
+      }
+      c.code_index = CalculateCodeIndex(code[c.char_index],  c.code_index,
+                                        start_index);
+      if (c.code_index >= static_cast<int>(code[c.char_index].length())) {
+        continue;  // 'abc def' ~ 'Ad'
+      }
+      if (c.code_index < 0) {
+        continue;  // 'abc def' ~ 'Ax'
+      }
+      if ((current.char_index < 0 || current.code_index < 0) &&
+          c.char_index == encoded.char_index &&
+          c.code_index <= encoded.code_index &&
+          (current.char_index != previous.char_index ||
+           current.code_index != previous.code_index)) {
+        continue;  // 'abc def' ~ '(AaBb)By', '(AaBb)Zb', '(AaZb)Zy'
+        // 'abc def' ~ '(AaZb)Zb' is OK
+        // 'abc def' ~ '(AaZb)Zz' is OK
+      }
+      *result += code[c.char_index][c.code_index];
+      previous = current;
+      encoded = c;
+    }
+    if (result->empty()) {
+      continue;
+    }
+    return true;
+  }
+
+  return false;
+}
+
+int TableEncoder::CalculateCodeIndex(const std::string& code, int index,
+                                     int start) {
+  // tail_anchor = '|'
+  const int n = static_cast<int>(code.length());
+  int k = 0;
+  if (index < 0) {
+    // 'ab|cd|ef|g' ~ '(Aa)Az' -> 'ac'; start = 1, index = -1
+    // 'ab|cd|ef|g' ~ '(AaAbAc)Az' -> 'abce'; start = 4, index = -1
+    // 'ab|cd|ef|g' ~ '(AaAbAc)Ay' -> 'abcd'; start = 4, index = -2
+    k = n - 1;
+    size_t tail = code.find_first_of(tail_anchor_, start);
+    if (tail != std::string::npos && static_cast<int>(tail) + 1 < k) {
+      k = static_cast<int>(tail) + 1;
+    }
+    while (++index < 0) {
+      while (--k >= 0 &&
+             tail_anchor_.find(code[k]) != std::string::npos) {
+      }
+    }
+  }
+  else {
+    // 'ab|cd|ef|g' ~ '(AaAb)Ac' -> 'abc'; index = 2
+    while (index-- > 0) {
+      while (++k < n &&
+             tail_anchor_.find(code[k]) != std::string::npos) {
+      }
+    }
+  }
+  return k;
+}
+
+}  // namespace rime
