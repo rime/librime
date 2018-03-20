d50b0182dcb98f2d7c37193dd6871c8666144d8e
Chen Gong <chen.sst@gmail.com>
2018-03-20 12:23:44 +0800

    chore(ci): build kyotocabinet

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -4,6 +4,7 @@ if [[ "$TRAVIS_OS_NAME" == linux ]]; then
   sudo apt-get update
   sudo apt-get install doxygen libboost-filesystem-dev libboost-locale-dev libboost-regex-dev libboost-signals-dev libboost-system-dev libgoogle-glog-dev libleveldb-dev libmarisa-dev libyaml-cpp-dev -y
   make thirdparty/gtest
+  make thirdparty/kyotocabinet
   make -C thirdparty/src/opencc build
   sudo env "PATH=$PATH" make -C thirdparty/src/opencc install
 elif [[ "$TRAVIS_OS_NAME" == osx ]]; then
