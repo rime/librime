a8ead411063b8cd9cd094745672123352a618630
Prcuvu <prcuvu@gmail.com>
2018-12-28 22:34:41 +0800

    chore(ci): replace AppVeyor boost download source

--- appveyor.install.bat
+++ appveyor.install.bat
@@ -7,12 +7,12 @@ if not exist %BOOST_ROOT% set nocache=1
 
 if %nocache% == 1 (
 	pushd C:\Libraries
-	appveyor DownloadFile http://cfhcable.dl.sourceforge.net/project/boost/boost/1.61.0/boost_1_61_0.7z
-	7z x boost_1_61_0.7z | find "ing archive"
-	cd boost_1_61_0
+	appveyor DownloadFile https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.7z
+	7z x boost_1_68_0.7z | find "ing archive"
+	cd boost_1_68_0
 	call .\bootstrap.bat
 	call .\b2.exe --prefix=%BOOST_ROOT% toolset=msvc-14.0 variant=release link=static threading=multi runtime-link=static define=BOOST_USE_WINAPI_VERSION=0x0501 cxxflags="/Zc:threadSafeInit- " --with-date_time --with-filesystem --with-locale --with-regex --with-signals --with-system --with-thread -q -d0 install
-	xcopy /e /i /y /q %BOOST_ROOT%\include\boost-1_61\boost %BOOST_ROOT%\boost
+	xcopy /e /i /y /q %BOOST_ROOT%\include\boost-1_68\boost %BOOST_ROOT%\boost
 	popd
 	if %ERRORLEVEL% NEQ 0 goto ERROR
 
