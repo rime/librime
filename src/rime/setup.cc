d43ef28fb0e2be40230b1edc0c2cff97ba569d7d
Chen Gong <chen.sst@gmail.com>
2019-03-05 22:09:01 +0800

    chore(CMakeLists.txt): rename macro RIME_EXTRA_MODULES

--- src/rime/setup.cc
+++ src/rime/setup.cc
@@ -11,13 +11,18 @@
 
 #include <rime_api.h>
 #include <rime/module.h>
+#include <rime/setup.h>
 
 namespace rime {
+
 #define Q(x) #x
-RIME_API RIME_MODULE_LIST(kDefaultModules, "default" RIME_EXTRA_DEFAULT_MODULES);
+RIME_API RIME_MODULE_LIST(kDefaultModules, "default" RIME_EXTRA_MODULES);
 #undef Q
 RIME_MODULE_LIST(kDeployerModules, "deployer");
 RIME_MODULE_LIST(kLegacyModules, "legacy");
+
+RIME_REGISTER_MODULE_GROUP(default, "core", "dict", "gears")
+RIME_REGISTER_MODULE_GROUP(deployer, "core", "dict", "levers")
 
 RIME_API void LoadModules(const char* module_names[]) {
   ModuleManager& mm(ModuleManager::instance());
@@ -33,8 +38,5 @@ RIME_API void SetupLogging(const char* app_name) {
   google::InitGoogleLogging(app_name);
 #endif  // RIME_ENABLE_LOGGING
 }
-
-RIME_REGISTER_MODULE_GROUP(default, "core", "dict", "gears")
-RIME_REGISTER_MODULE_GROUP(deployer, "core", "dict", "levers")
 
 }  // namespace rime
