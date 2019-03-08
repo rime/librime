2d8ba4548325b8d6468cb3e3c4145eca63e0bd67
Chen Gong <chen.sst@gmail.com>
2019-03-08 21:06:46 +0800

    refactor(cmake, plugins): BUILD_SEPARATE_LIBS by module, plus rime-plugins library or separate plugin libraries

---
+++ plugins/plugin.cc
@@ -0,0 +1,3 @@
+// Empty file to work around a CMake issue.
+// Somehow a C++ source file (in addition to rime_plugins_objs) is required
+// by CMake (3.13.3) to create shared rime-plugins library on macOS.
