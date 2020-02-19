a64f2e023dac3fca8410300a69f32dc672339ba7
Windham Wong <windhamwong@nva-hk.com>
2020-02-20 00:58:28 +0800

    Adding schema menu/page_down_cycle for setting page cycle i.e. Pressing space bar back to page 1 when reaching the last page of selection

--- src/rime/schema.h
+++ src/rime/schema.h
@@ -26,6 +26,7 @@ class Schema {
   void set_config(Config* config) { config_.reset(config); }
 
   int page_size() const { return page_size_; }
+  int page_down_cycle() const { return page_down_cycle_; }
   const string& select_keys() const { return select_keys_; }
   void set_select_keys(const string& keys) { select_keys_ = keys; }
 
@@ -37,6 +38,7 @@ class Schema {
   the<Config> config_;
   // frequently used config items
   int page_size_ = 5;
+  bool page_down_cycle_ = false;
   string select_keys_;
 };
 
