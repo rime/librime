bb2a616255d110cd288b022292c3102ccb408a16
Chen Gong <chen.sst@gmail.com>
2020-09-20 12:33:36 +0800

    build(appveyor): invalidate build cache when thirdparty modules are updated  [skip travis]

--- appveyor_build_thirdparty.bat
+++ appveyor_build_thirdparty.bat
@@ -4,11 +4,20 @@ set nocache=0
 
 if not exist thirdparty.cached set nocache=1
 
+git submodule status > thirdparty.submodule-status.new
+if not exist thirdparty.submodule-status (
+  set nocache=1
+) else (
+  fc thirdparty.submodule-status thirdparty.submodule-status.new
+  if errorlevel 1 set nocache=1
+)
+
 if %nocache% == 1 (
 	git submodule update --init
 	call .\build.bat thirdparty
 	if errorlevel 1 goto error
 
+	copy /y thirdparty.submodule-status.new thirdparty.submodule-status
 	date /t > thirdparty.cached & time /t >> thirdparty.cached
 	echo.
 	echo Thirdparty libraries installed.
