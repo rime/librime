7677dae996bdbf44f02e454cb46e70a4aab33cc3
Chen Gong <chen.sst@gmail.com>
2022-01-15 12:54:58 +0800

    fix(ci): install Rime plugins' dependencies
    macOS build and Windows build bundle plugins.
    Rime plugins may install their own dependencies in action-install.sh
    or action-install.bat in the plugin's top directory.
    
    deprecate travis ci config.

--- action-install-macos.sh
+++ action-install-macos.sh
@@ -7,4 +7,14 @@ export BOOST_ROOT="$(pwd)/thirdparty/src/boost_1_75_0"
 
 make xcode/thirdparty
 
+if [[ -n "${RIME_PLUGINS}" ]]; then
+    # intentionally unquoted: ${RIME_PLUGINS} is a space separated list of slugs
+    bash ./install-plugins.sh ${RIME_PLUGINS}
+    for plugin_dir in plugins/*; do
+        if [[ -e "${plugin_dir}/action-install.sh" ]]; then
+	        (cd "${plugin_dir}"; bash ./action-install.sh)
+        fi
+    done
+fi
+
 make xcode
