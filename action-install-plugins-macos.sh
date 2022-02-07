c9d231d57608c8d66b158aabc3014199a1f06b56
Chen Gong <chen.sst@gmail.com>
2022-02-07 09:05:22 +0800

    ci: upload artifacts

--- action-install-plugins-macos.sh
+++ action-install-plugins-macos.sh
@@ -2,10 +2,15 @@
 
 export RIME_ROOT="$(cd "$(dirname "$0")"; pwd)"
 
+echo "RIME_PLUGINS=${RIME_PLUGINS}" > version-info.txt
+echo "librime $(git describe --always)" >> version-info.txt
+
 if [[ -n "${RIME_PLUGINS}" ]]; then
     # intentionally unquoted: ${RIME_PLUGINS} is a space separated list of slugs
     bash ./install-plugins.sh ${RIME_PLUGINS}
     for plugin_dir in plugins/*; do
+        [[ -d "${plugin_dir}" ]] || continue
+        echo "${plugin_dir} $(git -C "${plugin_dir}" describe --always)" >> version-info.txt
         if [[ -e "${plugin_dir}/action-install.sh" ]]; then
 	        (cd "${plugin_dir}"; bash ./action-install.sh)
         fi
--- action-install-plugins-windows.bat
+++ action-install-plugins-windows.bat
@@ -2,18 +2,24 @@ setlocal
 
 if not defined RIME_ROOT set RIME_ROOT=%CD%
 
+echo RIME_PLUGINS=%RIME_PLUGINS% > version-info.txt
+echo librime >> version-info.txt
+git describe --always >> version-info.txt
+
 if defined RIME_PLUGINS (
-   for %%s in (%RIME_PLUGINS%) do call :install_plugin %%s || exit /b
+  for %%s in (%RIME_PLUGINS%) do call :install_plugin %%s || exit /b
 )
 exit /b
 
 :install_plugin
 set slug=%1
-echo "plugin: %slug%"
+echo plugin: %slug%
 set plugin_project=%slug:*/=%
 set plugin_dir=plugins\%plugin_project:librime-=%
 git clone --depth 1 "https://github.com/%slug%.git" %plugin_dir%
 if errorlevel 1 exit /b
+echo %plugin_dir% >> version-info.txt
+git -C %plugin_dir% describe --always >> version-info.txt
 if exist %plugin_dir%\action-install.bat (
   pushd %plugin_dir%
   call action-install.bat
