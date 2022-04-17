e9b4774e60031c443e4fc591243b3840f2a470a0
居戎氏 <chen.sst@gmail.com>
2022-04-17 23:50:13 +0800

    ci(env.vs2022.bat): build configuration for VS2022

---
+++ env.vs2022.bat
@@ -0,0 +1,15 @@
+rem Customize your build environment and save the modified copy to env.bat
+
+set RIME_ROOT=%CD%
+
+rem REQUIRED: path to Boost source directory
+if not defined BOOST_ROOT set BOOST_ROOT=%RIME_ROOT%\thirdparty\src\boost_1_76_0
+
+rem OPTIONAL: architecture, Visual Studio version and platform toolset
+set ARCH=Win32
+set BJAM_TOOLSET=msvc-14.3
+set CMAKE_GENERATOR="Visual Studio 17 2022"
+set PLATFORM_TOOLSET=v143
+
+rem OPTIONAL: path to additional build tools
+rem set DEVTOOLS_PATH=%ProgramFiles%\Git\cmd;%ProgramFiles%\CMake\bin;C:\Python27;
