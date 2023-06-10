fe5a65deb1348cd1b45702ded0041029add6dadd
居戎氏 <chen.sst@gmail.com>
2023-06-11 00:25:59 +0800

    build(env.bat.template): set default envvar
    these envvar used to have default values in build.bat.
    now they must be set in env.bat.

--- env.vs2022.bat
+++ env.vs2022.bat
@@ -5,7 +5,7 @@ set RIME_ROOT=%CD%
 rem REQUIRED: path to Boost source directory
 if not defined BOOST_ROOT set BOOST_ROOT=%RIME_ROOT%\deps\boost_1_78_0
 
-rem OPTIONAL: architecture, Visual Studio version and platform toolset
+rem architecture, Visual Studio version and platform toolset
 set ARCH=Win32
 set BJAM_TOOLSET=msvc-14.3
 set CMAKE_GENERATOR="Visual Studio 17 2022"
