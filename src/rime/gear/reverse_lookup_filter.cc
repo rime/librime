rime/librime

commit f079c3c5522acdcbdac7f01f449f57435df95a52

feat: add `append_comment` option to `reverse_lookup_filter` (#699)
Allow reverse_lookup_filter to append new codes to existing comments

src/rime/gear/reverse_lookup_filter.cc
@@ -53,6 +53,7 @@ void ReverseLookupFilter::Initialize() {
   }
   if (Config* config = engine_->schema()->config()) {
     config->GetBool(name_space_ + "/overwrite_comment", &overwrite_comment_);
+    config->GetBool(name_space_ + "/append_comment", &append_comment_);
     comment_formatter_.Load(config->GetList(name_space_ + "/comment_format"));
   }
 }
@@ -69,7 +70,7 @@ an<Translation> ReverseLookupFilter::Apply(an<Translation> translation,
 }
 
 void ReverseLookupFilter::Process(const an<Candidate>& cand) {
-  if (!overwrite_comment_ && !cand->comment().empty())
+  if (!cand->comment().empty() && !(overwrite_comment_ || append_comment_))
     return;
   auto phrase = As<Phrase>(Candidate::GetGenuineCandidate(cand));
   if (!phrase)
@@ -78,7 +79,11 @@ void ReverseLookupFilter::Process(const an<Candidate>& cand) {
   if (rev_dict_->ReverseLookup(phrase->text(), &codes)) {
     comment_formatter_.Apply(&codes);
     if (!codes.empty()) {
-      phrase->set_comment(codes);
+      if (overwrite_comment_) {
+        phrase->set_comment(codes);
+      } else if (append_comment_) {
+        phrase->set_comment(cand->comment() + " " + codes);
+      }
     }
   }
 }

src/rime/gear/reverse_lookup_filter.h
@@ -34,6 +34,7 @@ class ReverseLookupFilter : public Filter, TagMatching {
   the<ReverseLookupDictionary> rev_dict_;
   // settings
   bool overwrite_comment_ = false;
+  bool append_comment_ = false;
   Projection comment_formatter_;
 };
 

commit 8af548fd3eb3641993a55dc01f2ff4b82bd1d570

Fix a regression in reverse_lookup_filter introduced in #699  (#701)
---------

Co-authored-by: Qijia Liu <liumeo@pku.edu.cn>

src/rime/gear/reverse_lookup_filter.cc
@@ -79,9 +79,9 @@ void ReverseLookupFilter::Process(const an<Candidate>& cand) {
   if (rev_dict_->ReverseLookup(phrase->text(), &codes)) {
     comment_formatter_.Apply(&codes);
     if (!codes.empty()) {
-      if (overwrite_comment_) {
+      if (overwrite_comment_ || cand->comment().empty()) {
         phrase->set_comment(codes);
-      } else if (append_comment_) {
+      } else {
         phrase->set_comment(cand->comment() + " " + codes);
       }
     }