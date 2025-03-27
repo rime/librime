23e82e327fe919f7708196a60a4ae6f4ef7e5856
ustc.hj@gmail.com <ustc.hj@gmail.com>
2025-03-27 19:54:57 +0800

    Put the initialization code in GlobalEnvironment::SetUp and run the DLL-exported RimeInitialize in GlobalEnvironment::TearDown.
    https://github.com/rime/librime/pull/997#discussion_r2015752380

--- test/rime_test_main.cc
+++ test/rime_test_main.cc
@@ -1,31 +1,29 @@
 #include <gtest/gtest.h>
 #include <rime_api.h>
+#include <rime_api_impl.h>
 #include <rime/service.h>
 #include <rime/module.h>
 #include <rime/setup.h>
 
 class GlobalEnvironment : public testing::Environment {
  public:
-  void TearDown() override {
-    rime::Service::instance().deployer().JoinMaintenanceThread();
-    rime::Service::instance().StopService();
-    rime::Registry::instance().Clear();
-    rime::ModuleManager::instance().UnloadModules();
+  void SetUp() override {
+    rime::SetupLogging("rime.test");
+
+    RIME_STRUCT(RimeTraits, traits);
+    // put all files in the working directory ($build/test).
+    traits.shared_data_dir = traits.user_data_dir = traits.prebuilt_data_dir =
+        traits.staging_dir = ".";
+
+    RimeInitialize(&traits);
   }
+
+  void TearDown() override { RimeFinalize(); }
 };
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
   testing::AddGlobalTestEnvironment(new GlobalEnvironment);
-
-  RIME_STRUCT(RimeTraits, traits);
-  // put all files in the working directory ($build/test).
-  traits.shared_data_dir = traits.user_data_dir = traits.prebuilt_data_dir =
-      traits.staging_dir = ".";
-  rime::SetupDeployer(&traits);
-  rime::SetupLogging("rime.test");
-  rime::LoadModules(rime::kDefaultModules);
-  rime::Service::instance().StartService();
 
   return RUN_ALL_TESTS();
 }
