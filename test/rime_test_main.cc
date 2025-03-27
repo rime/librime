32a32e5f576a57626341961b55d591053a8f3f8c
ustc.hj@gmail.com <ustc.hj@gmail.com>
2025-03-27 23:53:27 +0800

    Fix building on Windows.
    https://github.com/rime/librime/actions/runs/14105954380/job/39515240902?pr=997

--- test/rime_test_main.cc
+++ test/rime_test_main.cc
@@ -1,9 +1,14 @@
 #include <gtest/gtest.h>
 #include <rime_api.h>
-#include <rime_api_impl.h>
 #include <rime/service.h>
-#include <rime/module.h>
 #include <rime/setup.h>
+
+#ifndef _WIN32
+// this include breaks building on Windows:
+// src\rime_api_impl.h:25:22: error: dllimport cannot be applied to non-inline
+// function definition 25 | RIME_DEPRECATED void RimeSetup(RimeTraits* traits)
+#include <rime_api_impl.h>
+#endif
 
 class GlobalEnvironment : public testing::Environment {
  public:
@@ -15,10 +20,18 @@ class GlobalEnvironment : public testing::Environment {
     traits.shared_data_dir = traits.user_data_dir = traits.prebuilt_data_dir =
         traits.staging_dir = ".";
 
+#ifdef _WIN32
+    rime::SetupDeployer(&traits);
+    rime::LoadModules(rime::kDefaultModules);
+    rime::Service::instance().StartService();
+  }
+#else
+    // not working on Windows since <rime_api_impl.h> is not included
     RimeInitialize(&traits);
   }
 
   void TearDown() override { RimeFinalize(); }
+#endif
 };
 
 int main(int argc, char** argv) {
