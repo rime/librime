7cf7dc2b8109b6fc33268289aad4bdc159b65151
居戎氏 <chen.sst@gmail.com>
2025-03-28 14:31:59 +0800

    use rime 1.0 api for setup and teardown
    This reverts commit 32a32e5f576a57626341961b55d591053a8f3f8c.

--- test/rime_test_main.cc
+++ test/rime_test_main.cc
@@ -3,35 +3,20 @@
 #include <rime/service.h>
 #include <rime/setup.h>
 
-#ifndef _WIN32
-// this include breaks building on Windows:
-// src\rime_api_impl.h:25:22: error: dllimport cannot be applied to non-inline
-// function definition 25 | RIME_DEPRECATED void RimeSetup(RimeTraits* traits)
-#include <rime_api_impl.h>
-#endif
-
 class GlobalEnvironment : public testing::Environment {
  public:
   void SetUp() override {
-    rime::SetupLogging("rime.test");
-
     RIME_STRUCT(RimeTraits, traits);
     // put all files in the working directory ($build/test).
     traits.shared_data_dir = traits.user_data_dir = traits.prebuilt_data_dir =
         traits.staging_dir = ".";
-
-#ifdef _WIN32
-    rime::SetupDeployer(&traits);
-    rime::LoadModules(rime::kDefaultModules);
-    rime::Service::instance().StartService();
-  }
-#else
-    // not working on Windows since <rime_api_impl.h> is not included
-    RimeInitialize(&traits);
+    // for logging.
+    traits.app_name = "rime.test";
+    rime_get_api()->setup(&traits);
+    rime_get_api()->initialize(&traits);
   }
 
-  void TearDown() override { RimeFinalize(); }
-#endif
+  void TearDown() override { rime_get_api()->finalize(); }
 };
 
 int main(int argc, char** argv) {
