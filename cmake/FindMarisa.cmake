fb1539fa8e7de65eaf30242047cd68928e260c17
ghostflyby <ghostflyby+git@outlook.com>
2026-01-15 19:01:09 +0800

    fix: add support for Marisa package with fallback to MODULE finding

---
+++ cmake/FindMarisa.cmake
@@ -0,0 +1,17 @@
+find_path(MARISA_INCLUDE_DIR NAMES marisa.h)
+
+find_library(MARISA_LIBRARY NAMES marisa libmarisa)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Marisa
+  DEFAULT_MSG MARISA_LIBRARY MARISA_INCLUDE_DIR
+)
+if(MARISA_FOUND AND NOT TARGET Marisa::marisa)
+  add_library(Marisa::marisa UNKNOWN IMPORTED)
+  set_target_properties(Marisa::marisa PROPERTIES
+    IMPORTED_LOCATION "${MARISA_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${MARISA_INCLUDE_DIR}"
+  )
+endif()
+
+mark_as_advanced(MARISA_INCLUDE_DIR MARISA_LIBRARY)
