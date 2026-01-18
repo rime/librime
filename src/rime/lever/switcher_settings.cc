58e51c54f374612cba6bc78881278ec310d228b9
fxliang <fx.liang@outlook.com>
2026-01-19 07:32:03 +0800

    fix(SwitcherSettings):load schema list when default.custom.yaml is absent (#1126)
    SwitcherSettings::Load returns false if the patch file is absent; it should keep loading the schema list.
    user code should handle the case when there is no custom setting.

--- src/rime/lever/switcher_settings.cc
+++ src/rime/lever/switcher_settings.cc
@@ -19,8 +19,7 @@ SwitcherSettings::SwitcherSettings(Deployer* deployer)
     : CustomSettings(deployer, "default", "Rime::SwitcherSettings") {}
 
 bool SwitcherSettings::Load() {
-  if (!CustomSettings::Load())
-    return false;
+  auto ret = CustomSettings::Load();
   available_.clear();
   selection_.clear();
   hotkeys_.clear();
@@ -28,7 +27,7 @@ bool SwitcherSettings::Load() {
   GetAvailableSchemasFromDirectory(deployer_->user_data_dir);
   GetSelectedSchemasFromConfig();
   GetHotkeysFromConfig();
-  return true;
+  return ret;
 }
 
 bool SwitcherSettings::Select(Selection selection) {
