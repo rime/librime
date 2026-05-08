1300e568967feeeedd72028c7cb5ef9151f7fb37
居戎氏 <chen.sst@gmail.com>
2026-05-08 12:03:59 +0800

    fix(charset_filter): 不過濾漢字以外的符號
    CJK Compatibility、CJK Compatibility Forms 只包含符號

--- src/rime/gear/charset_filter.cc
+++ src/rime/gear/charset_filter.cc
@@ -26,9 +26,9 @@ bool is_extended_cjk(uint32_t ch) {
       (ch >= 0x31350 && ch <= 0x323AF) ||  // CJK Unified Ideographs Extension H
       (ch >= 0x2EBF0 && ch <= 0x2EE5F) ||  // CJK Unified Ideographs Extension I
       (ch >= 0x323B0 && ch <= 0x3347F) ||  // CJK Unified Ideographs Extension J
-      (ch >= 0x3300 && ch <= 0x33FF) ||    // CJK Compatibility
-      (ch >= 0xFE30 && ch <= 0xFE4F) ||    // CJK Compatibility Forms
-      (ch >= 0xF900 && ch <= 0xFAFF) ||    // CJK Compatibility Ideographs
+      // (ch >= 0x3300 && ch <= 0x33FF) ||  // CJK Compatibility
+      // (ch >= 0xFE30 && ch <= 0xFE4F) ||  // CJK Compatibility Forms
+      (ch >= 0xF900 && ch <= 0xFAFF) ||  // CJK Compatibility Ideographs
       (ch >= 0x2F800 &&
        ch <= 0x2FA1F))  // CJK Compatibility Ideographs Supplement
     return true;
