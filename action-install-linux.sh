951ce0ffdc5d37060a0ac49788f80446440bb320
Bambooin <ooverflow@outlook.com>
2026-09-24 23:06:42 +0800

    Upgrade boost to 1.92.0 (#1239)
    * chore: upgrade boost to 1.92.0
    
    This patch is verified both in Arch Linux and Trime.
    1. https://gitlab.archlinux.org/archlinux/packaging/packages/librime/-/commit/caf2857beee1dc78c59552a9cc15df42b8bb2558
    2. https://github.com/osfans/trime/pull/2143
    
    * chore: remove the workaround with Ubuntu 22.04

--- action-install-linux.sh
+++ action-install-linux.sh
@@ -12,7 +12,4 @@ dep_packages=(
 )
 
 sudo apt update
-# fix a package dependency bug in Ubuntu 22.04
-# https://bugs.launchpad.net/ubuntu/+source/google-glog/+bug/1991919
-# https://github.com/kadalu-tech/pkgs/pull/2/files#r1001042597
-sudo apt install -y libunwind-dev ninja-build ${dep_packages[@]}
+sudo apt install -y ${dep_packages[@]}
--- install-boost.sh
+++ install-boost.sh
@@ -3,13 +3,13 @@ set -ex
 
 RIME_ROOT="$(cd "$(dirname "$0")"; pwd)"
 
-boost_version="${boost_version=1.89.0}"
+boost_version="${boost_version=1.92.0}"
 
 BOOST_ROOT="${BOOST_ROOT=${RIME_ROOT}/deps/boost-${boost_version}}"
 
 boost_tarball="boost_${boost_version//./_}.tar.gz"
 download_url="https://archives.boost.io/release/${boost_version}/source/${boost_tarball}"
-boost_tarball_sha256sum="9de758db755e8330a01d995b0a24d09798048400ac25c03fc5ea9be364b13c93  ${boost_tarball}"
+boost_tarball_sha256sum="c4a3b310ddd2472416e091067166b0713be97c63f38c212c484ada022fd296ce  ${boost_tarball}"
 
 download_boost_source() {
     cd "${RIME_ROOT}/deps"
